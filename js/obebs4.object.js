872bc8911690f748a20cfcf6c9afedb4c6dad82f
oberocks <matt@mattmct.com>
2019-05-11 14:24:37 -0400

    Merged class assignement into attributes object

--- js/obebs4.object.js
+++ js/obebs4.object.js
@@ -1,14 +1,11 @@
 
 const OBEBS4 = function () {
     this.version = '0.0.1',
-    this.element = function (elemType, classes = false, elemText = false, attributes = false, nestedElem = false) {
+    this.element = function (elemType, elemText = false, attributes = false, nestedElem = false) {
         let elem = document.createElement(elemType);
         if (elemText) {
             let node = document.createTextNode(elemText);
             elem.appendChild(node);
-        }
-        if (classes) {
-            elem.classList = classes;
         }
         if (attributes) {
             if (typeof attributes === 'object' && attributes !== null) {
@@ -61,22 +58,22 @@ target.appendChild(div);
 */
 
 
-let h_1 = obebs4.element('h3', false, 'First Headline');
-let hr_1 = obebs4.element('hr', 'border-success');
-let p_1 = obebs4.element('p', false, 'This is the first sample paragraph.');
+let h_1 = obebs4.element('h3', 'First Headline');
+let hr_1 = obebs4.element('hr', false, {'class' : 'border-success'});
+let p_1 = obebs4.element('p', 'This is the first sample paragraph.');
 
-let h_2 = obebs4.element('h3', false, 'Second Headline');
-let hr_2 = obebs4.element('hr', 'border-success');
-let p_2 = obebs4.element('p', false, 'This is the second sample paragraph.');
+let h_2 = obebs4.element('h3', 'Second Headline');
+let hr_2 = obebs4.element('hr', false, {'class' : 'border-success'});
+let p_2 = obebs4.element('p', 'This is the second sample paragraph.');
 
-let column_1 = obebs4.element('div', 'col-md-6', false, false, [h_1, hr_1, p_1]);
-let column_2 = obebs4.element('div', 'col-md-6', false, false, [h_2, hr_2, p_2]);
+let column_1 = obebs4.element('div', false, {'class' : 'col-md-6'}, [h_1, hr_1, p_1]);
+let column_2 = obebs4.element('div', false, {'class' : 'col-md-6'}, [h_2, hr_2, p_2]);
 
-let row = obebs4.element('div', 'row justify-content-center', false, false, [column_1, column_2]);
+let row = obebs4.element('div', false, {'class' : 'row justify-content-center'}, [column_1, column_2]);
 
-let container = obebs4.element('div', 'container', false, false, row);
+let container = obebs4.element('div', false, {'class' : 'container'}, row);
 
-let container_fluid = obebs4.element('div', 'container-fluid bg-dark text-light py-5', false, false, container);
+let container_fluid = obebs4.element('div', false, {'class' : 'container-fluid bg-dark text-light py-5'}, container);
 
 const target = document.getElementById('obebs4-app');
 target.appendChild(container_fluid);
